f6ec4051d7bb622612b35dc89e96c8ec431e539e
Tamaki Komatsuzaki <tamaki.komatsuzaki@gmail.com>
2025-10-31 20:56:00 +0900

    Update

--- page18/sketch.js
+++ page18/sketch.js
@@ -1,31 +1,14 @@
 let angle = 0;
 let wobble = 0;
+let wobb;
 function setup() {
-  createCanvas(windowWidth, windowHeight).parent('sketch-container');
+  createCanvas(windowWidth, windowHeight);
+  wobb = new WobbleCircle(width/2, height/2,200);
 }
 
 function draw() {
   background('lightblue');
-  noFill();
-  fill(255,255,224,30);
-  stroke(60,179,113,random(50));
-  
-  let stepW = PI/15;
-  
-  let my = map(sin(wobble), -1, 1, -PI/60, PI/60);
-  let mx = map(cos(wobble), 1, -1, PI/60, -PI/60);
-  let  radius = map(angle,0,TWO_PI,PI,0);
-  let baseRadius = min(width, height) * 0.18;
-  beginShape();
-   for (let i= 0; i<TWO_PI+0.5;i+=stepW){
-     let r = baseRadius + sin(wobble + i * 3) * 5;
-     let tx = width/2 + cos(angle+i)*(r+radius);//r;
-     let ty = height/2 + sin(angle+i)*(r+radius);//r;
-     stroke('white');
-     curveVertex(tx+mx,ty+my);
-     circle(tx+mx, ty+my, random(15));    
-    }
-   endShape();
-   wobble += 0.1;
-   angle+=0.005;
+  wobb.show();
+  wobble += 0.1;
+  angle+=0.005;
 }
---
+++ page18/wobblecircle.js
@@ -0,0 +1,46 @@
+class WobbleCircle{
+  constructor(x,y,r){
+    this.x = x;
+    this.y = y;
+    this.r = r;
+    this.stretchX = 0;
+    this.stretchY = 0;
+    this.center = createVector(x, y);
+    this.stretch = createVector();
+    this.v1 = createVector();
+    this.v2 = createVector();
+  }
+  show(){
+    
+    noFill();
+    fill(255,255,224,30);
+    stroke(60,179,113,random(50));
+
+    let stepW = PI/15;
+
+    let my = map(sin(wobble), -1, 1, -PI/60, PI/60);
+    let mx = map(cos(wobble), 1, -1, PI/60, -PI/60);
+    let radius = map(angle,0,TWO_PI,PI,0);
+
+    beginShape();
+     for (let i= 0; i<TWO_PI+0.5;i+=stepW){
+       let r = this.r + sin(wobble + i * 3) * 5;
+       let tx = this.x + cos(angle+i)*(r+radius)*(1 + this.stretch.x / 50);
+       let ty = this.y + sin(angle+i)*(r+radius)*(1 + this.stretch.y / 50);
+             
+
+       stroke('white');
+       curveVertex(tx+mx,ty+my);
+       circle(tx+mx, ty+my, random(15));
+      }
+    endShape();
+
+    if(mouseIsPressed){
+      this.v1.set(pmouseX,pmouseY);
+      this.v2.set(mouseX,mouseY);
+      this.stretch = p5.Vector.sub(this.v2,this.v1).mult(3);
+    }else {
+      this.stretch.set(0,0);
+    }
+  }
+}
